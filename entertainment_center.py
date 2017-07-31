8007a6396f385577668415b9bd01ddbf7757c5fe
Luis Quintanilla <equalsgains@gmail.com>
2017-07-31 00:03:06 -0600

    added toy story and avatar movie instances

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,4 +4,10 @@ toy_story = media.Movie("Toy Story",
                         " A stoyr of a boy and his toys that come to life!",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://youtu.be/KYz2wyBy3kc")
-print(toy_story.storyline)
+# print(toy_story.storyline)
+avatar = media.Movie("Avatar", 
+                     "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://youtu.be/5PSNL1qE6VY"
+                    )
+print(avatar.storyline)
